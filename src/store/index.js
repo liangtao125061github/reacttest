97c834c3e5290163e134ef3e4ba099b11f6e320c
liangtao <liangtao@erwan365.com>
2021-02-03 19:28:58 +0800

    运行eject

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import {createStore, combineReducers} from 'redux';
-import * as login from '../views/login';
-import * as home from '../views/home';
-
-let store = createStore(
-  combineReducers({...login, ...home}),
-);
-
-export default store;
